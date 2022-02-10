bd3675073f7c3ba9bc910819b8197d74f428af04
Aumboon <unaweblanca@gmail.com>
2022-02-10 20:59:24 +0700

    การเคลื่อนที่แนวตรง

--- การเคลื่อนที่แนวตรง.cpp
+++ การเคลื่อนที่แนวตรง.cpp
@@ -8,12 +8,16 @@ double u;
 double t;
 double v;
 
-void findV();
+void findV1();
+void findU();
 void findS();
 void findS1();
 void findS2();
 void findV2();
 void findSS();
+void findA();
+void findA1();
+void findV0();
 
 int main(){
 
@@ -27,7 +31,7 @@ int main(){
     cout << "please select number ";
     cin >> x;
     if(x == 1){
-        findV();
+        findV1();
     }
     else if(x == 2){
         findS();
@@ -48,15 +52,23 @@ int main(){
     return 0;
 }
 
-void findV(){
+void findV1(){ //ไม่มีs
     cout << "v = u+at" << '\n';
-    cout << "u = "; cin >> u; 
-    cout << "a = "; cin >> a;
-    cout << "t = "; cin >> t;
+    cout << "u = " ; cin >> u; 
+    cout << "a = " ; cin >> a;
+    cout << "t = " ; cin >> t;
     cout << "v = " << u + a*t;
 }
 
-void findS(){
+void findU(){ //ไม่มีs
+    cout << "u = v-at" << '\n';
+    cout << "v = " ; cin >> v;
+    cout << "a = " ; cin >> a;
+    cout << "t = " ; cin >> t;
+    cout << "u = " << v - a*t;
+}
+
+void findS(){ //ไม่มีa
     cout << "s = ((u+v)/2)*t" << '\n';
     cout << "u = " ; cin >> u;
     cout << "v = " ; cin >> v;
@@ -64,7 +76,7 @@ void findS(){
     cout << "s = " << (u+v)/2 * t;
 }
 
-void findS1(){
+void findS1(){ //ไม่มีv
     cout << "s = (u*t)+(1/2)*a*t^2" << '\n';
     cout << "u = " ; cin >> u;
     cout << "t = " ; cin >> t;
@@ -72,7 +84,7 @@ void findS1(){
     cout << "s = " << u*t + 0.5*a*pow(t,2);
 }
 
-void findS2(){
+void findS2(){ //ไม่มีu
     cout << "s = (v*t)-(1/2)*a*t^2" << '\n';
     cout << "v = " ; cin >> v;
     cout << "t = " ; cin >> t;
@@ -80,7 +92,7 @@ void findS2(){
     cout << "s = " << v*t - 0.5*a*pow(t,2);
 }
 
-void findV2(){
+void findV2(){ //ไม่มีt
     cout << "v^2 = u^2+2*a*s" << '\n';
     cout << "u = " ; cin >> u;
     cout << "a = " ; cin >> a;
@@ -88,9 +100,39 @@ void findV2(){
     cout << "v^2 = " << pow(u,2) + 2*a*s;
 }
 
-void findSS(){
+void findSS(){ //ระยะทาง
     cout << "s = v*t" << '\n';
     cout << "v = " ; cin >> v;
     cout << "t = " ; cin >> t;
     cout << "s = " << v*t;
 }
+
+void findA(){ //ความเร่ง
+    cout << "a = (v-u)/t" << '\n';
+    cout << "v = " ; cin >> v;
+    cout << "u = " ; cin >> u;
+    cout << "t = " ; cin >> t;
+    cout << "a = " << (v-u)/t;
+}
+
+void findA1(){ // อัตราเร่ง
+    cout << "a = s/t" << '\n';
+    cout << "s = " ; cin >> s;
+    cout << "t = " ; cin >> t;
+    cout << "a = " << s/t;
+}
+
+void findV0(){ //ไม่มีความเร่ง,ความเร็วคงที่
+    cout << "v = s/t" << '\n';
+    cout << "s = " ; cin >> s;
+    cout << "t = " ; cin >> t;
+    cout << "v = " << s/t;
+}
+
+void findt(){ //หาเวลาในการเคลื่อนที่
+    cout << "t = s/v" << '\n';
+    cout << "s = " ; cin >> s;
+    cout << "v = " ; cin >> v;
+    cout << "t = " << s/v;
+}
+
